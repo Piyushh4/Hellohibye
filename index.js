693bda8a51122637d52ca6d43a869011e67355db
Piyushh4 <120736971+Piyushh4@users.noreply.github.com>
2024-03-28 16:59:53 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Praying to God Choco🛐🍫","Playing Chess♟️"];
 
 
 let currentIndex = 0;
